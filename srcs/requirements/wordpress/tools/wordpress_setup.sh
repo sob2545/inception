fee5c1befe782fecf03d883f385d9d306d1bef1a
sob2545 <sob2545@naver.com>
2023-01-09 17:01:13 +0900

    FIX: wordpress_setup.sh

--- srcs/requirements/wordpress/tools/wordpress_setup.sh
+++ srcs/requirements/wordpress/tools/wordpress_setup.sh
@@ -8,12 +8,10 @@ if [ ! -d "/var/www" ];then
 	mv -f /tmp/wp-config.php /var/www/wordpress/
 	chown -R www-data:www-data /var/www/wordpress/
 
-	mv -f /tmp/www.conf /etc/php7.3/fpm/pool.d/www.conf
+	mv -f /tmp/www.conf /etc/php/7.3/fpm/pool.d/www.conf
 
 	mkdir -p /run/php/
 fi
 
 sleep 3
 echo "wordpress begin!"
-
-exec "$@"
